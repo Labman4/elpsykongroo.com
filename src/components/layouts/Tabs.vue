ae9949320a1872a6942bcb98bebdf8b5dfa2fac1
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-09-09 07:26:42 +0800

    fix

--- src/components/layouts/Tabs.vue
+++ src/components/layouts/Tabs.vue
@@ -3,9 +3,15 @@
       <el-tab-pane label="Home" name="Home">
           <Home/>
       </el-tab-pane>
-      <el-tab-pane label="Blog" name="Blog"></el-tab-pane>
+      <!-- <el-tab-pane label="Blog" name="Blog"></el-tab-pane>
       <el-tab-pane label="Daily" name="Daily"></el-tab-pane>
-      <el-tab-pane label="Else" name="Else"></el-tab-pane>
+      <el-tab-pane label="Else" name="Else">
+        <div id="paypal"></div>
+      </el-tab-pane> -->
+      <!-- <el-tab-pane label="Dashboard" name="Dashboard">
+        <Dashboard class="hidden-sm-and-down"/>
+        <span class="hidden-md-and-up">not support</span>
+      </el-tab-pane> -->
       <el-tab-pane label="Dashboard" name="Dashboard" v-if="visible.width > 768 && access.permission != undefined && access.permission.includes('admin')">
         <Dashboard class="hidden-sm-and-down"/>
         <span class="hidden-md-and-up">not support</span>
@@ -16,8 +22,8 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { access } from '~/assets/ts/access';
-import '~/assets/ts/live2d.js';
 import { visible } from '~/assets/ts/visible';
 
-const activeName = ref('Home')
+// import { loadPaypal } from "~/assets/ts/paypal"
+const activeName = ref('Home');
 </script>
