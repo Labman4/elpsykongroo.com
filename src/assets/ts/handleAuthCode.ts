f0d40b392bff502c79261787622fafcb1fdbbcbc
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-09-21 00:02:46 +0800

    fix sw register

--- src/assets/ts/handleAuthCode.ts
+++ src/assets/ts/handleAuthCode.ts
@@ -25,66 +25,80 @@ const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
 
 const register = async(username) => {
-  console.log("start register")
     if ("serviceWorker" in navigator) {
       // registerSW();
-      navigator.serviceWorker
-        .register(    
-          import.meta.env.VITE_PWA_MODE === 'production' ? '/firebase-messaging-sw.js' : '/dev-sw.js?dev-sw',
-          { type: import.meta.env.VITE_PWA_MODE  === 'production' ? 'classic' : 'module' }
-        )
-        .then((registration) => {
-        if ("Notification" in window ) {
-          window.Notification.requestPermission().then((permission) => {
-              if (permission === 'granted') {
-                console.log("notice enable")
-                // navigator.serviceWorker.ready.then((registration) => {
-                //   registration.showNotification("register ready");
-                // });
-                  if ('PushManager' in window) {
-                    console.log("push enable")
-                      getToken(messaging, {
-                          vapidKey: env.publicKey,
-                          serviceWorkerRegistration : registration 
-                      })
-                      .then((currentToken) => {
-                        console.log("register token")
-                          access.registerToken = currentToken                       
-                          const fcmOption = {
-                              baseURL: env.messageUrl,
-                              url: "notice/register",
-                              method: "PUT",
-                              params: {
-                                  token: currentToken,
-                                  timestamp: Date.now(),
-                                  user: username
-                              },
-                              headers: {
-                                  "Content-Type": "application/json",
-                                  // "X-Xsrf-Token":  handleCsrf()
-                              },
-                              withCredentials: true               
-                            }
-                          const client = axios.create(fcmOption)
-                          axiosRetry(client, 
-                            { retries: 1, retryCondition: (error) => {
-                              if (error.response.status === 403) {
-                                return true
-                              }
-                              return false
-                              }  
-                            });
-                          client(fcmOption)
-                      })
-                  }
-              }
+      navigator.serviceWorker.getRegistration().then(registration => {
+        if (registration && registration.active) {
+          registration.active.addEventListener('statechange', event => {
+            if (event.target.state === 'activated') {
+              registerSw(username)
+            }
           });
-      }      
-    }).catch(function(error){
-      console.log("register failed");
-      }) 
+        } else {
+          registerSw(username)
+        }
+      });
+
   }
 } 
+
+const registerSw = (username) => {
+  console.log("start register")
+  navigator.serviceWorker
+    .register(    
+      import.meta.env.VITE_PWA_MODE === 'production' ? '/firebase-messaging-sw.js' : '/dev-sw.js?dev-sw',
+      { type: import.meta.env.VITE_PWA_MODE  === 'production' ? 'classic' : 'module' })
+    .then((registration) => {
+        if ("Notification" in window ) {
+            window.Notification.requestPermission().then((permission) => {
+          if (permission === 'granted') {
+            console.log("notice enable")
+            // navigator.serviceWorker.ready.then((registration) => {
+            //   registration.showNotification("register ready");
+            // });
+              if ('PushManager' in window) {
+                console.log("push enable")
+                  getToken(messaging, {
+                      vapidKey: env.publicKey,
+                      serviceWorkerRegistration : registration 
+                  })
+                  .then((currentToken) => {
+                    console.log("register token")
+                      access.registerToken = currentToken                       
+                      const fcmOption = {
+                          baseURL: env.messageUrl,
+                          url: "notice/register",
+                          method: "PUT",
+                          params: {
+                              token: currentToken,
+                              timestamp: Date.now(),
+                              user: username
+                          },
+                          headers: {
+                              "Content-Type": "application/json",
+                              // "X-Xsrf-Token":  handleCsrf()
+                          },
+                          withCredentials: true               
+                        }
+                      const client = axios.create(fcmOption)
+                      axiosRetry(client, 
+                        { retries: 1, retryCondition: (error) => {
+                          if (error.response.status === 403) {
+                            return true
+                          }
+                          return false
+                          }  
+                        });
+                      client(fcmOption)
+                  })
+              }
+          }
+      });
+    }})
+    .catch(function(error){
+        console.log("register failed");
+    }) 
+}
 
 onMessage(messaging, (payload) => {
   console.log(payload)
