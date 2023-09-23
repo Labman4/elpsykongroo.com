817a4f3e96aacaccbaff507a8414981543a4169a
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-09-24 03:52:46 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "~/styles/index.scss";
 import 'uno.css'
 import 'element-plus/theme-chalk/display.css';
 import 'virtual:svg-icons-register'
+import '~/assets/ts/live2d';
 
 const app = createApp(App)
 app.use(router);
