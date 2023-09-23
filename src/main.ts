7033994436867fd1b29f6142d51357d9529c4f5c
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-09-24 03:15:01 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from '~/router/router';
 import "~/styles/index.scss";
 import 'uno.css'
 import 'element-plus/theme-chalk/display.css';
+import 'virtual:svg-icons-register'
 
 const app = createApp(App)
 app.use(router);
