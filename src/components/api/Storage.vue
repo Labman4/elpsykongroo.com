8c89a80eb20e0735ba86729571342a44d2aa1c98
Labman4 <50023562+Labman4@users.noreply.github.com>
2024-02-04 02:28:59 +0800

    debug

--- src/components/api/Storage.vue
+++ src/components/api/Storage.vue
@@ -725,7 +725,8 @@ const saveS3Info = async() => {
 
 const loadS3Info = async(row: s3Info) => {
   if (s3Secret.value) {
-    if (access.platform == "cloudflare" && access.sub != "") {
+    console.log(access)
+    if (row.platform == "cloudflare" && access.sub != "") {
       await directPreflight()
     }
     initS3Info(row.accessKey, secretformRef)
